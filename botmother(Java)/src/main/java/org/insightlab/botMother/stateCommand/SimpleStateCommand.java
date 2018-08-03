32d856c9f6c564df4dc9947fb41a7329932b7116
Lucas Peres <lucaspg96@live.com>
2018-08-03 15:13:25 -0300

    Documenting classes from stateCommand package

--- botmother(Java)/src/main/java/org/insightlab/botMother/stateCommand/SimpleStateCommand.java
+++ botmother(Java)/src/main/java/org/insightlab/botMother/stateCommand/SimpleStateCommand.java
@@ -1,11 +1,58 @@
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Insight Data Science Lab
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package org.insightlab.botMother.stateCommand;
 
 import org.telegram.telegrambots.api.objects.Update;
 
+/**
+ * StateCommand that executes only for updates which text starts with a specific command.
+ * All commands start with '/'.
+ *
+ * @author Lucas Peres
+ * @since 1.0
+ */
 public abstract class SimpleStateCommand implements StateCommand {
 
+    /**
+     * Command text that will be used at match method.
+     */
 	protected String command;
 
+    /**
+     * Constructor that sets the command text.
+     * @param command
+     */
+	public SimpleStateCommand(String command){
+	    this.setCommand(command);
+    }
+
+    /**
+     * Set the command text
+     * @param command command that will be set.
+     */
 	public void setCommand(String command) {
 		if (command.startsWith("/"))
 			this.command = command;
@@ -13,6 +60,11 @@ public abstract class SimpleStateCommand implements StateCommand {
 			this.command = "/" + command;
 	}
 
+    /**
+     * Check if the update triggers the command
+     * @param update updated that will be tested.
+     * @return true if this command should process this update. False otherwise.
+     */
 	public boolean match(Update update) {
 		try {
 			return update.hasMessage() && update.getMessage().getText().split(" ")[0].equals(this.command);
--- botmother(Java)/src/main/java/org/insightlab/botMother/stateCommand/StateCommand.java
+++ botmother(Java)/src/main/java/org/insightlab/botMother/stateCommand/StateCommand.java
@@ -1,9 +1,53 @@
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Insight Data Science Lab
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
 package org.insightlab.botMother.stateCommand;
 
 import org.insightlab.botMother.bot.ConversationBot;
 import org.telegram.telegrambots.api.objects.Update;
 
+/**
+ * StateCommand interface used by the commands that are going to be inserted on a ConversationBot.
+ * @see org.insightlab.botMother.bot.ConversationBot
+ * @author Lucas Peres
+ * @since 1.0
+ */
 public interface StateCommand {
-	public Object run(Update update, ConversationBot bot);
-	public boolean match(Update update);
+    /**
+     * Executes the command
+     * @param update update that triggered the command.
+     * @param bot Bot object that received the update.
+     * @return the new state after this command execution.
+     */
+	Object run(Update update, ConversationBot bot);
+
+    /**
+     * Check if the update triggers the command
+     * @param update updated that will be tested.
+     * @return true if this command should process this update. False otherwise.
+     */
+	boolean match(Update update);
 }
